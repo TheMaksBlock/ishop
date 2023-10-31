88e406fecfeac41f95bfbd91760a61c4bd995d79
TheMaksBlock <maka228228@mail.ru>
2023-11-01 00:48:32 +0300

    added cashing

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 
+use ishop\Cash;
 use RedBeanPHP\R;
 
 class MainController extends App {
@@ -15,6 +16,16 @@ class MainController extends App {
         $posts = R::findAll('test');
         $this->setMeta(\ishop\App::$app->getPropertie('shop_name'), 'Описание', 'Ключи');
         $this->set(compact('posts'));
+        $cash = Cash::instance();
+        $cash->set('test', $posts);
+        $cash->delete('test');
+        $posts = R::findOne('test', 'id=?',[1]);
+        $data = $cash->get('test');
+        if(!$data){
+            $cash->set('test', $posts);
+        }
+        $data = $cash->get('test');
+        debug($data);
     }
 
 }
